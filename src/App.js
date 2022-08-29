c4dc8a2e25a456450f6e6a5aa609d87c422888c0
Mstierli37 <85417496+Mstierli37@users.noreply.github.com>
2022-08-29 19:53:29 -0300

    Consignas completadas

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import Listado from './components/Listado';
+import Cabecera from './components/Cabecera';
+import React from 'react';
 // El componente App es el padre de:
 // - Cabecera
 // - Listado
@@ -6,11 +9,17 @@
 // PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
 
 function App() {
+  
+  const [cont, setCont] = React.useState(0);
+
+  function increment () {
+    setCont(cont + 1);
+  }
 
   return (
     <div className="App">
-      <Cabecera />
-      <Listado />
+      <Cabecera cantidad={cont}/>
+      <Listado increment={increment}/>
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,4 +1,5 @@
 
+import React from 'react';
 // El componente Item no tiene componentes hijos.
 // ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
 // MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
@@ -9,11 +10,28 @@
 //    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
 //    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
 
-export default function Item() {
+export default function Item(props) {
+  
+  // eslint-disable-next-line no-unused-vars
+  const [stock] = React.useState(props.data.stock);
+
+  function handleClick () {
+    if(props.data.stock > 0){
+      props.increment();
+      props.data.stock -= 1;
+    }
+  }
 
   return (
     <div className='producto'>
-      {/* maquetar Item aquí */}
+      {
+        <>
+          <h3>{props.data.producto.nombre}</h3>
+          <p>{props.data.producto.descripcion}</p>
+          <h5>En stock: {props.data.stock !== 0 ? props.data.stock : <span>agotado</span>}</h5>
+          <button onClick={handleClick} disabled={props.data.stock <= 0}>{props.data.stock > 0 ? 'COMPRAR' : 'SIN STOCK'}</button>
+        </>
+      }
     </div>
   )
 }
